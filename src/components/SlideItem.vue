1feb6ebd289752e44a79924af752be0f30704a2d
kgodlewska <katarzyna-godlewska1@wp.pl>
2022-09-08 17:16:21 +0000

    create slide item component

---
+++ src/components/SlideItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="slide">
+    <div class="slide__content">
+      <h1 class="slide__title">
+        Shine up on <span class="slide__title--black-background">Diamond Beach</span> with icebergs
+      </h1>
+      <p class="slide__description">
+        Magical place where icebergs flowing glacier lagoon
+        <span class="slide__description--bold">Jökulsárlón</span> throught the black beach to the Atlantic Ocean
+      </p>
+      <button class="slide__button">Read more</button>
+    </div>
+    <img src="../assets/diamond-beach.png" class="slide__img" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SlideItem"
+}
+</script>
+
+<style lang="scss">
+</style>
